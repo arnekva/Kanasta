71b49d65a89fc9c2b0717ab02ebc96b5bba2a5b8
Arne Kvaleberg <arnekva@gmail.com>
2020-06-27 21:24:36 +0200

    updated rules

--- game.js
+++ game.js
@@ -2,18 +2,67 @@ let playerCount = 0;
 let players = [];
 let rounds = {
 "rounds":[
-  {"name": "3+3", "rules":"3 like + 3 like"},
-  {"name": "3+4", "rules":"3 like + 1 serie"},
-  {"name": "4+4", "rules":"2 serier"},
-  {"name": "3+3+3", "rules":"3 like + 3 like + 3 like"},
-  {"name": "3+3+4", "rules":"3 like + 3 like + 1 serie"},
-  {"name": "3+4+4", "rules":"3 like + 2 serier"},
-  {"name": "4+4+4", "rules":"3 serier"},
-  {"name": "3+3+3+3", "rules":"3 like 4 ganger"},
+  {"name": "3+3", "rules":"3 like + 3 like", "antall":"6"},
+  {"name": "3+4", "rules":"3 like + 1 serie","antall":"7"},
+  {"name": "4+4", "rules":"2 serier","antall":"8"},
+  {"name": "3+3+3", "rules":"3 like + 3 like + 3 like","antall":"9"},
+  {"name": "3+3+4", "rules":"3 like + 3 like + 1 serie", "antall":"10"},
+  {"name": "3+4+4", "rules":"3 like + 2 serier", "antall":"11"},
+  {"name": "4+4+4", "rules":"3 serier","antall":"12"},
+  {"name": "3+3+3+3", "rules":"3 like 4 ganger", "antall":"12"},
 ]
 }
 let roundCount = 0;
+let hasJustRestored = false;
+function checkForState(){
+  let state = restoreState();
+  if(state !== null){
+    console.log("not null");
+    hideCreationPart();
+    firstRoundSetup();
 
+    startNextRound();
+  }
+}
+checkForState();
+
+function saveState(){
+  let state = {
+    "players":players,
+    "roundCount":roundCount,
+
+  }
+  let saved = JSON.stringify(state)
+  localStorage.setItem('state', saved)
+  console.log("Saved to storage");
+}
+function restoreState(){
+  hasJustRestored = true;
+  console.log("Attempting to retrieve state from storage");
+  let state = JSON.parse(localStorage.getItem('state'))
+  if(state !== null){
+    playerCount = state.players.length
+    console.log("Found previous state");
+    console.log(state);
+    for(let i = 0; i<state.playerCount;i++){
+      let player = new Player(state.players[i].id, state.players[i].name)
+      player.updateRounds(state.players[i].rounds)
+    players[i] = state.players
+    console.log();
+    }
+    players = state.players;
+    roundCount = state.roundCount;
+    hideSteps();
+    printCurrentLeader()
+    printScores();
+    return state
+  }
+  return null
+}
+function nukeState(){
+localStorage.clear();
+location.reload()
+}
 function Player(id, name){
   this.id = id;
   this.name = name;
@@ -25,23 +74,28 @@ function Player(id, name){
   this.setRoundScore = function(score){
     this.rounds.push(score);
   }
+  this.updateRounds = function(newRounds){
+    this.rounds = newRounds;
+  }
 
 }
-
+function hideSteps(){
+  let btn = document.getElementById("startBtn")
+  let inp = document.getElementById("playerInputCount")
+  let text = document.getElementById("task")
+  let gmh = document.getElementById("gameHeader")
+  gmh.style.display = "none"
+  text.innerText = "Skriv inn navnet på spillerene"
+  inp.style.display = "none"
+  btn.style.display = "none"
+}
 function startGame(){
   let input = document.getElementById("playerInputCount").value
   //Check input for number
     playerCount = input;
     createNameInputFields(input);
-    let btn = document.getElementById("startBtn")
-    let inp = document.getElementById("playerInputCount")
-    let text = document.getElementById("task")
-    let gmh = document.getElementById("gameHeader")
-    gmh.style.display = "none"
-    text.innerText = "Skriv inn navnet på spillerene"
-    inp.style.display = "none"
-    btn.style.display = "none"
 
+    hideSteps();
 }
 
 function createNameInputFields(count){
@@ -59,8 +113,8 @@ let node = document.getElementById("nameInputs")
     let names = document.getElementsByClassName("nameInputField")
     for(let i = 0; i<playerCount;i++){
       let navn = names[i].value;
-      if(navn.length > 14){
-        navn = navn.substr(1,13) + "..."
+      if(navn.length > 12){
+        navn = navn.substr(1,10) + "..."
       }
     let player = new Player(names[i].dataset.playerid, navn)
     players.push(player)
@@ -80,17 +134,24 @@ function hideCreationPart(){
 }
 
 function startNextRound(){
+  saveState();
   let header = document.getElementById('roundHeader');
+  let cardCount = document.getElementById('antallKort');
+  let roundCountText = document.getElementById('roundCount');
   let ruleText = document.getElementById('roundRules')
   header.innerText = rounds.rounds[roundCount].name
   ruleText.innerText = rounds.rounds[roundCount].rules
-  if(roundCount === 0){
+  cardCount.innerText = "Det skal deles " + rounds.rounds[roundCount].antall + " kort"
+  let runde = roundCount - 0 +1
+  roundCountText.innerText = "Runde " + runde + " av " + rounds.rounds.length
+  if(roundCount === 0 && hasJustRestored){
     firstRoundSetup()
     }
 
 }
 function firstRoundSetup(){
   let node = document.getElementById("scoreInputContainer")
+    console.log("COUNT: " + playerCount);
   for(let i = 0; i<playerCount;i++){
     let container = document.createElement('div')
     container.className = "playerContainer"
@@ -106,12 +167,13 @@ function firstRoundSetup(){
   }
 
   let submitScoresButton = document.createElement('input')
+  //todo: legg til event listener
   submitScoresButton.onclick = function(){
     let values = document.getElementsByClassName('roundScoreField')
       for(let i = 0; i<playerCount;i++){
         for(let j = 0; j<=playerCount;j++){
         if(values[j].dataset.playerid === players[i].id){
-          players[i].setRoundScore(values[j].value - 0)
+          players[i].rounds.push(values[j].value - 0)
           players[i].score += values[j].value - 0;
         }
         }
@@ -151,7 +213,7 @@ function clearFields(){
   els[0].focus()
 }
 function printScores(){
-  if(roundCount == 0){
+  if(roundCount == 0 || (hasJustRestored && roundCount>0)){
   let scoreContainer = document.getElementById("currentScoreContainer")
   for(let i = 0; i<playerCount;i++){
     let scoreText = document.createElement("p")
@@ -159,6 +221,7 @@ function printScores(){
     scoreText.innerText = players[i].name + " har " + players[i].score + " poeng."
     scoreContainer.appendChild(scoreText)
     }
+    hasJustRestored = false;
   }
   let scoreTexts = document.getElementsByClassName('scoreText');
   for(let i = 0; i<playerCount;i++){
